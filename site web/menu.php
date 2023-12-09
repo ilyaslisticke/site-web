40dbab0f6d0b2db75ae0f92665ccadcdd2114a61
Ilyas Keurti <keurtiilyas@gmail.com>
2023-12-09 12:53:52 +0100

    first commit

---
+++ site web/menu.php
@@ -0,0 +1,14 @@
+<nav class="navbar" >
+    <a href="#" class="logo" style="color: #89c8c8;" >test suii</a>
+    <div class="nav">
+        <ul >
+            <li style="color: #89c8c8;"><a href="site web.html">home</a></li>
+            <li style="color: #89c8c8;"><a href="site webP2.html">contact</a></li>
+            <li style="color: #89c8c8;"><a href="#">info</a></li>
+            <li style="color: #89c8c8;"><a href="#">brevet</a></li>
+            <li style="color: #89c8c8;"><a href="#">test</a></li>
+        </ul>
+    </div>
+    <img src="../image/menu icon vrai vrai wlh" 
+    alt="menu hamburger" class="hamburger">
+</nav>
